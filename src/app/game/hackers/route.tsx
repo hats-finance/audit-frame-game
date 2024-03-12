1ca5c6ff821ec01fcbd076b3ff54dc9dd79182e8
Carlos Fontes <fonstack@gmail.com>
2024-03-12 18:57:56 +0000

    removed unused library

--- src/app/game/hackers/route.tsx
+++ src/app/game/hackers/route.tsx
@@ -4,8 +4,6 @@ import { IFarcasterUser, IProfileData } from "@/data/models";
 import { getGamePoints } from "@/helpers/getGamePoints";
 import { COLORS } from "@/consts/colors";
 import { ipfsTransformUri } from "@/helpers/ipfsTransformUri";
-// @ts-ignore
-import Identicon from "react-identicons";
 import { config } from "@/config/config";
 import { HACKER_HEIGHT, HACKER_MARGIN, HACKER_WIDTH } from "@/consts/hackersUI";
 
@@ -82,74 +80,100 @@ export async function GET(request: Request) {
             width: 900,
             margin: "auto",
           }}>
-          {hackers.map((hacker) => (
-            <div
-              key={hacker.username}
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                height: HACKER_HEIGHT,
-                width: HACKER_WIDTH,
-                margin: `0 ${HACKER_MARGIN}px 40px`,
-                color: "white",
-                borderRadius: 10,
-                fontFamily: "IBMPlexSans",
-                background: "linear-gradient(135deg, #24E8C5, #F782FF, #816FFF)",
-              }}>
+          {hackers.map((hacker) => {
+            return (
               <div
+                key={hacker.username}
                 style={{
                   display: "flex",
                   flexDirection: "column",
-                  height: HACKER_HEIGHT - 4,
-                  width: HACKER_WIDTH - 4,
-                  background: COLORS.background,
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: HACKER_HEIGHT,
+                  width: HACKER_WIDTH,
+                  margin: `0 ${HACKER_MARGIN}px 40px`,
+                  color: "white",
                   borderRadius: 10,
-                  padding: "10px 25px",
+                  fontFamily: "IBMPlexSans",
+                  background: "linear-gradient(135deg, #24E8C5, #F782FF, #816FFF)",
                 }}>
-                <div style={{ display: "flex", justifyContent: "space-between" }}>
-                  <div style={{ display: "flex", flexDirection: "column" }}>
-                    <p style={{ fontSize: "40px", fontFamily: "IBMPlexSansBold", margin: 0 }}>#{hacker.idx}</p>
-                    <p style={{ fontSize: "34px", fontFamily: "IBMPlexSansBold", marginBottom: 25, marginTop: 8 }}>
-                      {hacker.username}
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    height: HACKER_HEIGHT - 4,
+                    width: HACKER_WIDTH - 4,
+                    background: COLORS.background,
+                    borderRadius: 10,
+                    padding: "10px 25px",
+                  }}>
+                  <div style={{ display: "flex", justifyContent: "space-between" }}>
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                      <p style={{ fontSize: "40px", fontFamily: "IBMPlexSansBold", margin: 0 }}>#{hacker.idx}</p>
+                      <p style={{ fontSize: "34px", fontFamily: "IBMPlexSansBold", marginBottom: 15, marginTop: 5 }}>
+                        {hacker.username}
+                      </p>
+                    </div>
+
+                    {/* Profile image */}
+                    <div
+                      style={{ display: "flex", borderRadius: 1000, overflow: "hidden", width: 90, height: 90, marginTop: 10 }}>
+                      {hacker.avatar ? (
+                        <img src={ipfsTransformUri(hacker.avatar)} width={90} height={90} />
+                      ) : hacker.github ? (
+                        <img src={`https://github.com/${hacker.github}.png`} width={90} height={90} />
+                      ) : (
+                        <img src={`${config.hostURL}/assets/icons/identicon.png`} width={90} height={90} />
+                      )}
+                    </div>
+                  </div>
+
+                  {hacker.leaderboardPlace !== 0 ? (
+                    <p style={{ fontSize: "28px", margin: 0, display: "flex", alignItems: "center", marginBottom: 2 }}>
+                      <span style={{ fontFamily: "IBMPlexSansBold", marginRight: 5 }}>{hacker.leaderboardPlace}st</span> on the
+                      leaderboard
                     </p>
-                  </div>
+                  ) : (
+                    <p style={{ fontSize: "28px", margin: 0, display: "flex", alignItems: "center", marginBottom: 2 }}>
+                      not on the leaderboard
+                    </p>
+                  )}
 
-                  {/* Profile image */}
-                  <div style={{ display: "flex", borderRadius: 1000, overflow: "hidden", width: 90, height: 90, marginTop: 10 }}>
-                    {hacker.avatar ? (
-                      <img src={ipfsTransformUri(hacker.avatar)} width={90} height={90} />
-                    ) : hacker.github ? (
-                      <img src={`https://github.com/${hacker.github}.png`} width={90} height={90} />
-                    ) : (
-                      <img src={`${config.hostURL}/assets/icons/identicon.png`} width={90} height={90} />
-                    )}
-                  </div>
-                </div>
+                  {hacker.leaderboardPlace !== 0 && (
+                    <p style={{ fontSize: "25px", margin: 0, marginBottom: 2, color: COLORS.greyText }}>
+                      ${millify(hacker.totalAmountRewards ?? 0)} total earnings
+                    </p>
+                  )}
 
-                <p style={{ fontSize: "28px", margin: 0, display: "flex", alignItems: "center", marginBottom: 6 }}>
-                  <span style={{ fontFamily: "IBMPlexSansBold", marginRight: 5 }}>{hacker.leaderboardPlace}st</span> on the
-                  leaderboard
-                </p>
-                <p style={{ fontSize: "25px", margin: 0, marginBottom: 6, color: COLORS.greyText }}>
-                  ${millify(hacker.totalAmountRewards ?? 0)} total earnings
-                </p>
-                <p style={{ fontSize: "25px", margin: 0, marginBottom: 5 }}>{hacker.totalFindings} issues found</p>
-                <div style={{ fontSize: "30px", display: "flex", alignItems: "center", justifyContent: "center" }}>
-                  <p
+                  {hacker.leaderboardPlace !== 0 && (
+                    <p style={{ fontSize: "25px", margin: 0, marginBottom: 5 }}>{hacker.totalFindings} issues found</p>
+                  )}
+
+                  <div
                     style={{
-                      color: "#24E8C5",
-                      fontFamily: "IBMPlexSansBold",
-                      marginRight: 5,
+                      fontSize: "30px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      position: "absolute",
+                      bottom: -10,
+                      left: "40%",
+                      margin: 0,
                     }}>
-                    50
-                  </p>
-                  votes
+                    <p
+                      style={{
+                        color: "#24E8C5",
+                        fontFamily: "IBMPlexSansBold",
+                        marginRight: 5,
+                      }}>
+                      {hacker.voters ?? 0}
+                    </p>
+                    votes
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
 
         <div style={{ display: "flex", position: "absolute", bottom: 30, right: "50%", transform: "translateX(50%)" }}>
